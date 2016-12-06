bbfafef544c9cc2d8b843031051fe08360ed6340
mc007 <asd28071977>
2016-12-06 14:19:37 +0100

    =jsdoc for dojo/dnd

--- dnd/Target.js
+++ dnd/Target.js
@@ -1,8 +1,15 @@
-define([ "../_base/declare", "../dom-class", "./Source" ], function(declare, domClass, Source){
-	return declare("dojo.dnd.Target", Source, {
-		// summary:
-		//		a Target object, which can be used as a DnD target
-
+/** @module dojo/dnd/Target **/
+define([
+    "../_base/declare",
+    "../dom-class",
+    "./Source"
+], function(declare, domClass, Source){
+    /**
+     * A Target object, which can be used as a DnD target
+     * @class module:dojo/dnd/Target
+     * @extends module:dojo/dnd/Source
+     */
+    return declare("dojo.dnd.Target", Source, {
 		constructor: function(/*===== node, params =====*/){
 			// summary:
 			//		a constructor of the Target --- see the `dojo/dnd/Source` constructor for details
